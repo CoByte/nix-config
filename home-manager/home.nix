a62345853eaaa864cbb1680a5f299e65899c4cef
cobyte <owen356wh@gmail.com>
2025-01-05 22:30:29 -0800

    before nvim migration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -49,18 +49,91 @@
   home.homeDirectory = "/home/raine";
 
   # Add stuff for your user as you see fit:
-  programs.neovim.enable = true;
   programs.kitty.enable = true;
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # disable greeting
+
+      # launch tmux on startup
+      if type -q tmux
+          if not test -n "$TMUX"
+              tmux attach-session -t default; or tmux new-session -s default
+          end
+      end
+    '';
+    plugins = [
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+    ];
+  };
+
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    shortcut = "a";
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+
+    extraConfig = ''
+      # enable mouse
+      set-option -g mouse on
+
+      # unbind old splits
+      unbind %
+      unbind '"'
+
+      # simple split-pane commands
+      bind \\ split-window -h -c '#{pane_current_path}'
+      bind - split-window -v -c '#{pane_current_path}'
+      bind c split-window -c '#{pane_current_path}'
+
+      # fullscreen
+      bind -r m resize-pane -Z
+    '';
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.helix = {
+    enable = true;
+
+    settings = {
+      editor.cursor-shape = {
+        normal = "block";
+	insert = "bar";
+	select = "underline";
+      };
+    };
+
+    languages.language = [
+      {
+        name = "nix";
+	auto-format = true;
+	formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+    ];
+  };
 
   home.packages = with pkgs; [
     # applications
     spotify
     discord
+    obsidian
 
     # random garbage
     neo-cowsay
     toybox
     gnome-tweaks
+    grc
+    nix-prefetch-github
   ];
 
   # Enable home-manager and git
