0a20a39acdb28fe7fc414a499a33b27761ad4973
cobyte <32520644+CoByte@users.noreply.github.com>
2025-02-02 23:37:14 -0800

    removed helix (i dont use it

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -199,26 +199,6 @@
     '';
   };
 
-  programs.helix = {
-    enable = true;
-
-    settings = {
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
-      };
-    };
-
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-      }
-    ];
-  };
-
   home.packages = with pkgs; [
     # applications
     spotify
