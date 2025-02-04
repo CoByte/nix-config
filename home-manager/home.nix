72fa2a8f14b288f4850e28c9dc9409bd025d1d4d
cobyte <32520644+CoByte@users.noreply.github.com>
2025-02-03 22:38:00 -0800

    modified pretty-fold config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -128,7 +128,7 @@
       vp.nvim-web-devicons
       (minimalPlugin vp.bufferline-nvim "bufferline")
       (configdPlugin vp.lualine-nvim ./nvim/plugins/lualine.lua)
-      (minimalPlugin vp.pretty-fold-nvim "pretty-fold")
+      # (minimalPlugin vp.pretty-fold-nvim "pretty-fold")
 
       # treesitter
       {
