63083d6d6ffc03daa814b204c3ab867a9fea36a4
cobyte <32520644+CoByte@users.noreply.github.com>
2025-02-05 10:22:30 -0800

    updated fzf defaults to work a little better

--- home-manager/fish/config.fish
+++ home-manager/fish/config.fish
@@ -4,8 +4,10 @@ set fish_greeting # disable greeting
 abbr --add nsf nix-shell --command fish
 abbr --add cdgr "cd (git rev-parse --show-toplevel)"
 
-# make fzf work properly w/ tmux
+# env
 set -x FZF_DEFAULT_OPTS '--height 40% --tmux bottom,40% --layout reverse --border top'
+set -x FZF_CTRL_T_OPTS '--walker-skip .git,.steam'
+set -x FZF_ALT_C_OPTS '--walker-skip .git,.steam'
 
 # launch tmux on startup
 if type -q tmux
