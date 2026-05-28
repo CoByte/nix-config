5dbcdd46aeebc1e76ebbece0d9dbf20205626bd3
cobyte <32520644+CoByte@users.noreply.github.com>
2026-05-28 12:16:08 -0700

    shiny and new pt 1

--- flake.nix
+++ flake.nix
@@ -3,24 +3,21 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # installing as a flake for system-level programs (bootloaders, etc.)
     stylix = {
-      url = "github:danth/stylix/release-24.11";
-      inputs.nixpkgs.follows = "home-manager";
+      url = "github:danth/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # assorted inputs
-    j-link.url = "github:liff/j-link-flake";
   };
 
   outputs = {
@@ -29,7 +26,6 @@
     nixpkgs-unstable,
     home-manager,
     stylix,
-    j-link,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -67,7 +63,6 @@
       specialArgs = {inherit inputs outputs;};
       modules = [
         stylix.nixosModules.stylix
-        j-link.nixosModule
         ./nixos/configuration.nix
       ];
     };
